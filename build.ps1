d06310f83cd64412c7c053d5bbdff075b22f760a
bparks13 <bap@open-ephys.org>
2024-07-31 14:51:52 -0400

    Update submodule references - Remove onix-refactor reference - Add onix-bonsai-onix1 reference - Remove bonsai reference - Fix paths for source code and artifacts

--- build.ps1
+++ build.ps1
@@ -1,8 +1,8 @@
 # Build project
-$libPath = ".\src\onix-refactor\OpenEphys.Onix\"
+$libPath = ".\src\onix-bonsai-onix1\"
 dotnet build $libPath --configuration Release
 
 # Export workflow vectors
-$libPath = Join-Path $libPath "OpenEphys.Onix\bin\x64\Release\net472"
+$libPath = Join-Path $libPath "artifacts\bin\OpenEphys.Onix\release"
 .\docfx-tools\modules\Export-Image.ps1 -bootstrapperPath .\.bonsai\Bonsai.exe $libPath
 dotnet docfx @args
