cf143ab1dca01e5726fcca2824bf570db2c9343d
cjsha <cs@open-ephys.org>
2024-08-20 18:32:20 -0400

    Resolve issues 10 & 32
    Besides resolving the issues, misc changes:
    - update src code submodule pointer
    - filter out a couple files that were causing broken references from
      OpenEphys.Onix1.Design
    - .css instead of javascript to remove whitespace in tables
    - add image for transfoermer-operatore.svg which I made for Shawn
    - add xcf & webp and write text for Bonsai package uninstallation instructions
    - use mref.ext.js to add code tags to first of tables contained in remarks
    - sort properties using the sort higher order method
    - address PR review from Brandon & edit the device configuration
      operator disclaimer

--- template/ManagedReference.extension.js
+++ template/ManagedReference.extension.js
@@ -1,12 +1,10 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-// replace last instance of '<p' with '<p style="margin-bottom:0;"'
-function removeBottomMargin(str) {
-  return str
-    .split('').reverse().join('')
-    .replace('<p'.split('').reverse().join(''), '<p style="margin-bottom:0;"'.split('').reverse().join(''))
-    .split('').reverse().join('');
+function addCodeTag(remarks) {
+  return remarks
+    .replace('<td class=\"term\"> True </td>', '<td class=\"term\"><code> True </code></td>')
+    .replace('<td class=\"term\"> False </td>', '<td class=\"term\"><code> False </code></td>');
 }
 
 function extractEnumData(model) {
@@ -14,40 +12,16 @@ function extractEnumData(model) {
     .filter(child => child.type === 'field')
     .map(child => ({
       'field&value': child.syntax.content[0].value,
-      'enumDescription': removeBottomMargin([child.summary, child.remarks].join(''))
+      'enumDescription': [child.summary, child.remarks].join(''),
     }));
 }
 
 function sortPropertiesData(properties) {
-
-  const propertyNames = properties.map(property => {
-    return [property.name,
-    /\D+\d+$/.test(property.name),
-    String(property.name.match(/\D+/)),
-    Number(property.name.match(/\d+$/))
-    ]
-  });
-
-  let propertyNamesThatFitPattern = []
-  propertyNames
-    .filter(propertyName => propertyName[1])
-    .forEach(propertyName => { if (!propertyNamesThatFitPattern.includes(propertyName[2])) { propertyNamesThatFitPattern.push(propertyName[2]); } });
-  
-  const propertiesLength = properties.length;
-  const propertyNamesThatFitPatternLength = propertyNamesThatFitPattern.length;
-  for (let i = 0; i < propertyNamesThatFitPatternLength; i++) {
-    for (let j = 1; j < propertiesLength; j++) {
-      for (let k = j; k > 0; k--) {
-        if ((propertyNames[k][2] === propertyNamesThatFitPattern[i]) && (propertyNames[k - 1][2] === propertyNamesThatFitPattern[i])) {
-          if (propertyNames[k][3] < propertyNames[k - 1][3]) {
-            [properties[k], properties[k - 1]] = [properties[k - 1], properties[k]];
-            [propertyNames[k], propertyNames[k - 1]] = [propertyNames[k - 1], propertyNames[k]];
-          }
-        }
-      }
-    }
-  }
-  return properties;
+  return properties.sort((a, b) => (
+    a.name.match(/\D+|\d+$/g)[0] === b.name.match(/\D+|\d+$/g)[0] ?
+      Number(a.name.match(/\D+|\d+$/g)[1]) - Number(b.name.match(/\D+|\d+$/g)[1]) :
+      0
+  ));
 }
 
 function processChildProperty(child, sharedModel) {
@@ -58,9 +32,7 @@ function processChildProperty(child, sharedModel) {
     'name': child.name[0].value,
     'type': child.syntax.return.type.specName[0].value,
     'propertyDescription': {
-      'text': enumFields.length > 0
-        ? [child.summary, child.remarks].join('')
-        : removeBottomMargin([child.summary, child.remarks].join('')),
+      'text': addCodeTag([child.summary, child.remarks].join('')),
       'hasEnum': enumFields.length > 0,
       'enum': enumFields,
     }
@@ -74,15 +46,14 @@ function extractPropertiesData(model, sharedModel) {
 }
 
 function extractPropertiesFromInheritedMembersData(model, sharedModel) {
-
   return model.inheritedMembers
     .filter(inheritedMember => inheritedMember.type === 'property')
-    .map(inheritedMember => {
-      return processChildProperty(
+    .map(inheritedMember => (
+      processChildProperty(
         sharedModel[`~/api/${inheritedMember.parent}.yml`].children.find(inheritedMemberChild => inheritedMemberChild.uid === inheritedMember.uid),
         sharedModel
-      );
-    });
+      )
+    ));
 }
 
 function extractConstituentOperatorsData(model) {
@@ -121,8 +92,8 @@ function extractOperatorData(model) {
     .filter(child => child.name[0].value.includes('Process') || child.name[0].value.includes('Generate'))
     .map(child => ({
       'overloadsDescription': [child.summary, child.remarks].join(''),
-      'input': removeBottomMargin([child.syntax?.parameters[0].description, child.syntax?.parameters[0].remarks].join('')),
-      'output': removeBottomMargin([child.syntax.return.description, child.syntax.return.remarks].join('')),
+      'input': [child.syntax?.parameters[0].description, child.syntax?.parameters[0].remarks].join(''),
+      'output': [child.syntax.return.description, child.syntax.return.remarks].join(''),
     }));
 
   return {
@@ -149,8 +120,9 @@ exports.preTransform = function (model) {
         'subProperties': sortPropertiesData([
           ...extractPropertiesData(model, model.__global._shared),
           ...extractPropertiesFromInheritedMembersData(model, model.__global._shared),
-        ]).filter(modelProperty => !properties.map(property => property.object).includes(modelProperty.name))
+        ]).filter(modelProperty => !properties.map(property => property.object).includes(modelProperty?.name))
       });
+      console.log(JSON.stringify(properties, null, 2));
     }
     else {
       properties = sortPropertiesData([
